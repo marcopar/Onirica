b4dad63c58b9401568f522f16bc60e7033a2a95e
marcopar <marcopar@gmail.com>
2025-03-03 21:22:00 +0100

    Godot 4.4, better shades for nightmare buttons and labyrinth

--- project/scenes/nightmare_panel/nightmare_button.gd
+++ project/scenes/nightmare_panel/nightmare_button.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var icon: Sprite2D = $Icon
 
 @export var texture: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite_2d.texture = texture
+	icon.texture = texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
