71e23abcb16f43477804dab51e4625df62ac3150
marcopar <marcopar@gmail.com>
2025-03-03 22:47:54 +0100

    Nightmare button selected logic

--- project/scenes/nightmare_panel/nightmare_button.gd
+++ project/scenes/nightmare_panel/nightmare_button.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
 @onready var icon: Sprite2D = $Icon
+@onready var background: Sprite2D = $Background
 
 @export var texture: Texture2D
+
+var selected: bool = false:
+	get:
+		return selected
+	set(value):		
+		selected = value
+		if selected:
+			background.self_modulate = Color(1,1,0)
+		else:
+			background.self_modulate = Color(1,1,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +23,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			if selected:
+				selected = false
+			else:
+				selected = true
