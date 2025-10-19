f4b1e8dadb561c304d6c17ef244bb63e12b17551
marcopar <marcopar@gmail.com>
2025-10-19 11:54:08 +0100

    Nighmare counter

--- project/scenes/deck_counter/deck_counter.gd
+++ project/scenes/deck_counter/deck_counter.gd
@@ -1,15 +1,21 @@
-extends Node2D
+extends Control
 
 class_name DeckCounter
 
-@onready var label: Label = $Label
+@onready var nightmare_count: Label = $HBoxContainer/NightmareCount
+@onready var card_count: Label = $HBoxContainer/CardCount
 
 func _ready() -> void:
 	SignalManager.new_game.connect(new_game)
 	SignalManager.deck_updated.connect(deck_updated)
 	
 func new_game() -> void:
-	label.text = "%02d" % GameManager.deck_model.get_number_of_cards()
+	update_labels()	
 	
 func deck_updated() -> void:
-	label.text = "%02d" % GameManager.deck_model.get_number_of_cards()
+	update_labels()
+	
+func update_labels() -> void:
+	card_count.text = "%02d" % GameManager.deck_model.get_number_of_cards()
+	nightmare_count.text = "%02d" % GameManager.deck_model.get_number_of(CardManager.CARD_TYPE.NIGHTMARE)
+	
