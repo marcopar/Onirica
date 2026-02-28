91d5ac7357660df2e5664c9be0058ee9447c96f0
marcopar <marcopar@gmail.com>
2026-02-28 16:09:41 +0100

    Rules screen

--- project/scenes/rules_screen/rules.gd
+++ project/scenes/rules_screen/rules.gd
@@ -1,11 +1,11 @@
 extends Control
 
-class_name Credits
+class_name Rules
 
-func _on_exit_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:	
 	AudioManager.play_uiclick_sound()
 	SceneManager.switch_to_menu()
-
-func _on_credits_text_meta_clicked(meta: Variant) -> void:
+	
+func _on_rules_text_meta_clicked(meta: Variant) -> void:
 	AudioManager.play_uiclick_sound()
 	OS.shell_open(str(meta))
