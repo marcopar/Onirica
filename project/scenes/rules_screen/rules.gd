80e0991e1c7544109e45177475ca42fa7eca6e4a
marcopar <marcopar@gmail.com>
2026-03-13 21:31:48 +0100

    WIP

--- project/scenes/rules_screen/rules.gd
+++ project/scenes/rules_screen/rules.gd
@@ -6,6 +6,3 @@ func _on_exit_button_pressed() -> void:
 	AudioManager.play_uiclick_sound()
 	SceneManager.switch_to_menu()
 	
-func _on_rules_text_meta_clicked(meta: Variant) -> void:
-	AudioManager.play_uiclick_sound()
-	OS.shell_open(str(meta))
---
+++ project/scenes/rules_screen/rules_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+class_name RulesMenu
+
+func _on_base_game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_the_glyphs_pressed() -> void:
+	pass # Replace with function body.
