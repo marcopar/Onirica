576a1b6ead06c6c24ff78a8e3261a216d82f2603
marcopar <marcopar@gmail.com>
2026-04-26 12:52:44 +0200

    Always show deck counters unless the deck is a trigger of an action, interection avriant selection and combo check (#81)

--- project/scenes/expansion_selection_screen/expansion_selection.gd
+++ project/scenes/expansion_selection_screen/expansion_selection.gd
@@ -4,6 +4,7 @@ class_name ExpansionSelection
 
 @onready var the_glyphs: Button = $"GUI/VBoxContainer/MainArea/MarginContainer/VBoxContainer/TheGlyphs"
 @onready var crossroads_and_ded_ends: Button = $GUI/VBoxContainer/MainArea/MarginContainer/VBoxContainer/CrossroadsAndDedEnds
+@onready var intersections: Button = $GUI/VBoxContainer/MainArea/MarginContainer/VBoxContainer/Intersections
 
 func _ready() -> void:
 	update_labels()
@@ -24,20 +25,36 @@ func _on_crossroads_and_ded_ends_pressed() -> void:
 	AudioManager.play_uiclick_sound()
 	if GameManager.crossroads_and_dead_ends_on:
 		GameManager.crossroads_and_dead_ends_on = false
+		GameManager.intersections_on = false
 	else:
 		GameManager.crossroads_and_dead_ends_on = true
+	update_labels()
+
+func _on_intersections_pressed() -> void:
+	AudioManager.play_uiclick_sound()
+	if GameManager.intersections_on:
+		GameManager.intersections_on = false
+	else:
+		GameManager.intersections_on = true
 	update_labels()
 	
 func update_labels() -> void:
 	if GameManager.the_glyphs_on == false:
-		the_glyphs.text = "THE GLYPHS OFF"
+		the_glyphs.modulate.v = 0.7
 	else:
-		the_glyphs.text = "THE GLYPHS ON"
+		the_glyphs.modulate.v = 1
 		
 	if GameManager.crossroads_and_dead_ends_on == false:
-		crossroads_and_ded_ends.text = "CROSSROADS AND DEAD ENDS OFF"
+		crossroads_and_ded_ends.modulate.v = 0.7
+		intersections.visible = false
 	else:
-		crossroads_and_ded_ends.text = "CROSSROADS AND DEAD ENDS ON"
+		crossroads_and_ded_ends.modulate.v = 1
+		intersections.visible = true
+		
+	if GameManager.intersections_on == false:
+		intersections.modulate.v = 0.7
+	else:
+		intersections.modulate.v = 1
 	
 func _on_start_game_pressed() -> void:
 	AudioManager.play_uiclick_sound()
