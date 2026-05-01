c2b08ed335e398f3547eba46074c0581465f068c
marcopar <marcopar@gmail.com>
2026-05-01 12:44:34 +0200

    New layout

--- project/scenes/deck_counter/deck_counter.gd
+++ project/scenes/deck_counter/deck_counter.gd
@@ -3,9 +3,8 @@ extends Control
 class_name DeckCounter
 
 @onready var card_count: Label = $HBoxContainer/CardCount
-@onready var nightmare_count: Label = $HBoxContainer/NightmareCount
-@onready var dead_end_count: Label = $HBoxContainer/DeadEndCount
-
+@onready var nightmare_count: Label = $HBoxContainer/VBoxContainer/NightmareCount
+@onready var dead_end_count: Label = $HBoxContainer/VBoxContainer/DeadEndCount
 
 func _ready() -> void:
 	dead_end_count.visible = GameManager.crossroads_and_dead_ends_on
