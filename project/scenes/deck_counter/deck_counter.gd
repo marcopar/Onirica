4454ecfe75ef371d94f7254bc82f5d163e8ff7cf
marcopar <marcopar@gmail.com>
2026-05-01 12:21:34 +0200

    Dead end counter

--- project/scenes/deck_counter/deck_counter.gd
+++ project/scenes/deck_counter/deck_counter.gd
@@ -2,10 +2,13 @@ extends Control
 
 class_name DeckCounter
 
-@onready var nightmare_count: Label = $HBoxContainer/NightmareCount
 @onready var card_count: Label = $HBoxContainer/CardCount
+@onready var nightmare_count: Label = $HBoxContainer/NightmareCount
+@onready var dead_end_count: Label = $HBoxContainer/DeadEndCount
+
 
 func _ready() -> void:
+	dead_end_count.visible = GameManager.crossroads_and_dead_ends_on
 	SignalManager.new_game.connect(new_game)
 	SignalManager.deck_updated.connect(deck_updated)
 	
@@ -18,4 +21,5 @@ func deck_updated() -> void:
 func update_labels() -> void:
 	card_count.text = "%02d" % GameManager.deck_model.get_number_of_cards()
 	nightmare_count.text = "%02d" % GameManager.deck_model.get_number_of(CardManager.CARD_TYPE.NIGHTMARE)
+	dead_end_count.text = "%02d" % GameManager.deck_model.get_number_of(CardManager.CARD_TYPE.DEAD_END)
 	
