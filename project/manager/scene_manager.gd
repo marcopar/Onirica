269b124cac57af2bc6292cde28b9577b24da6026
marcopar <marcopar@gmail.com>
2026-05-08 18:07:02 +0200

    Use call deferred to change scene

--- project/manager/scene_manager.gd
+++ project/manager/scene_manager.gd
@@ -1,28 +1,28 @@
 extends Node
 
 func switch_to_game() -> void:
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/main/main.tscn")
 	
 func switch_to_menu() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/main_menu/main_menu.tscn")
 
 func switch_to_credits() -> void:
-	get_tree().change_scene_to_file("res://scenes/credits_screen/credits.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/credits_screen/credits.tscn")
 	
 func switch_to_settings() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings_screen/settings.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/settings_screen/settings.tscn")
 	
 func switch_to_rules_menu() -> void:
-	get_tree().change_scene_to_file("res://scenes/rules_screen/rules_menu.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/rules_screen/rules_menu.tscn")
 	
 func switch_to_rules_base() -> void:
-	get_tree().change_scene_to_file("res://scenes/rules_screen/rules_base.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/rules_screen/rules_base.tscn")
 
 func switch_to_rules_the_glyphs() -> void:
-	get_tree().change_scene_to_file("res://scenes/rules_screen/rules_the_glyphs.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/rules_screen/rules_the_glyphs.tscn")
 	
 func switch_to_rules_crossroads_and_dead_ends() -> void:
-	get_tree().change_scene_to_file("res://scenes/rules_screen/rules_crossroads_and_dead_ends.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/rules_screen/rules_crossroads_and_dead_ends.tscn")
 	
 func switch_to_expansion_selection() -> void:
-	get_tree().change_scene_to_file("res://scenes/expansion_selection_screen/expansion_selection.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/expansion_selection_screen/expansion_selection.tscn")
